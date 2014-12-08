ad6c15cfc60f97a8feb0545499b88b6e48bf2e53
Jevon Wright <jevon@jevon.org>
2014-12-08 15:11:19 +1300

    Adding database and config

--- inc/global.php
+++ inc/global.php
@@ -1,3 +1,21 @@
 <?php
 
 require(__DIR__ . "/../vendor/autoload.php");
+
+Openclerk\Config::merge(array(
+  "database_name" => "clerk2",
+  "database_username" => "clerk2",
+  "database_password" => "clerk2",
+));
+
+function config($key, $default = null) {
+  return Openclerk\Config::get($key, $default);
+}
+
+function db() {
+  return new \Db\Connection(
+    config("database_name"),
+    config("database_username"),
+    config("database_password")
+  );
+}
--- site/index.php
+++ site/index.php
@@ -12,3 +12,34 @@ require(__DIR__ . "/../inc/global.php");
     echo "<li>[" . $key . "] " . $cur->getName() . " (" . get_class($cur) . ")</li>\n";
   } ?>
 </ul>
+
+<h2>Migrations</h2>
+
+<?php
+
+echo "<p>(" . implode(", ", DiscoveredComponents\Migrations::getKeys()) . ")</p>";
+
+class MyLogger extends \Db\Logger {
+  function log($s) {
+    echo "<li>" . htmlspecialchars($s) . "</li>";
+  }
+  function error($s) {
+    echo "<li class=\"error\" style=\"color:red;\">" . htmlspecialchars($s) . "</li>";
+  }
+}
+
+$logger = new MyLogger();
+
+class AllMigrations extends \Db\Migration {
+  function getParents() {
+    return array(new Db\BaseMigration()) + DiscoveredComponents\Migrations::getAllInstances();
+  }
+}
+
+$migrations = new AllMigrations(db());
+if ($migrations->hasPending(db())) {
+  echo "<h3>Installing migrations</h3>";
+  echo "<ul>";
+  $migrations->install(db(), $logger);
+  echo "</ul>";
+}
