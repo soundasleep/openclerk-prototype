2f7b61aeb31369c1ea04f12f06739abd013f714f
Jevon Wright <jevon@jevon.org>
2014-12-13 12:50:15 +1300

    Testing navigation page template extension

--- site/security/login.php
+++ site/security/login.php
@@ -2,6 +2,9 @@
 
 // page logic
 $user = Users\UserPassword::tryLogin(db(), "jevon@jevon.org", "jevon");
+if ($user) {
+  $user->persist(db());
+}
 
 // page render
 page_header("Login", "page_login");
--- templates/login/password.php
+++ templates/login/password.php
@@ -2,7 +2,6 @@
 
 if ($user) {
   echo "<h2>Logged in successfully as $user</h2>";
-  $user->persist(db());
 } else {
   echo "<h2>Could not log in</a>";
 }
